6d0e18a69cdd1bc54f92851877e55031d996afe8
mahesh bodhgire <mahesh.bodhgire@anblicks.com>
2022-10-27 10:10:50 +0530

    revision-c-programming

---
+++ revision-c-programing/firstCProgram.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main ()
+{
+    //veriable declaration/defination
+    int number;
+    //variable initialization
+    number = 123;
+    
+    float percentage;
+    percentage = 1.2;
+    
+    printf("My name is Krishna\n");
+    printf("Number = %d\n", number); //Number = 123
+    
+    //Percentage = 1.2
+    printf("Percentage = %f\n", percentage);
+    
+    //Number = 123, Percentage = 1.2
+    printf("Number = %d, Percentage = %f",number, percentage);
+  return 0;
+}
